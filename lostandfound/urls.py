f52dcb27f4fc5e2ed5058c6282540e4ba962deb7
kaitlyncapone1 <kcapone3@binghamton.edu>
2025-11-11 19:55:44 +0000

    Fixed admin issue

--- lostandfound/urls.py
+++ lostandfound/urls.py
@@ -10,7 +10,6 @@ from django.urls import path
 from lostandfound import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
